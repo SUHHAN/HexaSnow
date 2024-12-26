5f514c3db1095c34c188066b975dd9248de307fd
EunHaSong <shaeun03290@sookmyung.ac.kr>
2024-12-26 11:44:47 +0900

    refactor. 지원언니 googleManager 내용 주석 처리 해둠

--- Assets/Scripts/jiwon/GoogleManager.cs
+++ Assets/Scripts/jiwon/GoogleManager.cs
@@ -1,46 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using GooglePlayGames;
-using GooglePlayGames.BasicApi;
-using TMPro;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using GooglePlayGames;
+// using GooglePlayGames.BasicApi;
+// using TMPro;
 
-public class GoogleManager : MonoBehaviour
-{
-    public TextMeshProUGUI logText; 
+// public class GoogleManager : MonoBehaviour
+// {
+//     public TextMeshProUGUI logText; 
 
-    void Start()
-    {
-        PlayGamesPlatform.DebugLogEnabled = true;
-        PlayGamesPlatform.Activate();
-        SignIn();
-    }
+//     void Start()
+//     {
+//         PlayGamesPlatform.DebugLogEnabled = true;
+//         PlayGamesPlatform.Activate();
+//         SignIn();
+//     }
 
-    public void SignIn()
-    {
-        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
-    }
+//     public void SignIn()
+//     {
+//         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+//     }
 
-    internal void ProcessAuthentication(SignInStatus status)
-    {
-        if (status == SignInStatus.Success)
-        {
-            // Continue with Play Games Services
-            // Perfectly login success
+//     internal void ProcessAuthentication(SignInStatus status)
+//     {
+//         if (status == SignInStatus.Success)
+//         {
+//             // Continue with Play Games Services
+//             // Perfectly login success
 
-            string name = PlayGamesPlatform.Instance.GetUserDisplayName();
-            string id = PlayGamesPlatform.Instance.GetUserId();
-            string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+//             string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+//             string id = PlayGamesPlatform.Instance.GetUserId();
+//             string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
 
-            logText.text = "Success \n" + name;
-        }
-        else
-        {
-            logText.text = "Sign in Failed!";
-            // Disable your integration with Play Games Services or show a login button
-            // to ask users to sign-in. Clicking it should call
-            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
-            // Login failed
-        }
-    }
-}
+//             logText.text = "Success \n" + name;
+//         }
+//         else
+//         {
+//             logText.text = "Sign in Failed!";
+//             // Disable your integration with Play Games Services or show a login button
+//             // to ask users to sign-in. Clicking it should call
+//             // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+//             // Login failed
+//         }
+//     }
+// }
