15e1a6c154850339a7045bc73fcd341f0f9f6b87
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-01-04 23:41:00 +0900

    feat. button 예시 이미지 추가함
    images/Button 폴더 안에 존재함

--- Assets/Scripts/jiwon/GoogleManager.cs
+++ Assets/Scripts/jiwon/GoogleManager.cs
@@ -1,62 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using GooglePlayGames;
-using GooglePlayGames.BasicApi;
-using TMPro;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using GooglePlayGames;
+// using GooglePlayGames.BasicApi;
+// using TMPro;
 
-public class GoogleManager : MonoBehaviour
-{
-    public TextMeshProUGUI logText;
+// public class GoogleManager : MonoBehaviour
+// {
+//     public TextMeshProUGUI logText;
 
-    void Start()
-    {
-        Debug.Log("App Starting...");
-        Debug.Log("Enabling PlayGamesPlatform debug log.");
-        GooglePlayGames.OurUtils.Logger.DebugLogEnabled = true;
+//     void Start()
+//     {
+//         Debug.Log("App Starting...");
+//         Debug.Log("Enabling PlayGamesPlatform debug log.");
+//         GooglePlayGames.OurUtils.Logger.DebugLogEnabled = true;
 
-        Debug.Log("Activating PlayGamesPlatform.");
-        PlayGamesPlatform.Activate();
+//         Debug.Log("Activating PlayGamesPlatform.");
+//         PlayGamesPlatform.Activate();
 
-        Debug.Log("Calling SignIn...");
-        SignIn();
+//         Debug.Log("Calling SignIn...");
+//         SignIn();
         
-    }
+//     }
 
-    public void SignIn()
-    {
-        Debug.Log("Starting Authentication...");
-        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication, true);
-    }
+//     public void SignIn()
+//     {
+//         Debug.Log("Starting Authentication...");
+//         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication, true);
+//     }
 
-    internal void ProcessAuthentication(SignInStatus status)
-    {
-        Debug.Log("Authentication Status: " + status);
+//     internal void ProcessAuthentication(SignInStatus status)
+//     {
+//         Debug.Log("Authentication Status: " + status);
 
-        switch (status)
-        {
-            case SignInStatus.Success:
-                Debug.Log("Login successful.");
-                string name = PlayGamesPlatform.Instance.GetUserDisplayName();
-                string id = PlayGamesPlatform.Instance.GetUserId();
-                string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
-                logText.text = "Success \n" + name;
-                break;
+//         switch (status)
+//         {
+//             case SignInStatus.Success:
+//                 Debug.Log("Login successful.");
+//                 string name = PlayGamesPlatform.Instance.GetUserDisplayName();
+//                 string id = PlayGamesPlatform.Instance.GetUserId();
+//                 string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+//                 logText.text = "Success \n" + name;
+//                 break;
 
-            case SignInStatus.Canceled:
-                Debug.LogError("Login canceled by the user.");
-                logText.text = "Sign in Failed! Canceled by the user.";
-                break;
+//             case SignInStatus.Canceled:
+//                 Debug.LogError("Login canceled by the user.");
+//                 logText.text = "Sign in Failed! Canceled by the user.";
+//                 break;
 
-            case SignInStatus.InternalError:
-                Debug.LogError("Internal error occurred during login.");
-                logText.text = "Sign in Failed! Internal error.";
-                break;
+//             case SignInStatus.InternalError:
+//                 Debug.LogError("Internal error occurred during login.");
+//                 logText.text = "Sign in Failed! Internal error.";
+//                 break;
 
-            default:
-                Debug.LogError("Unknown error: " + status);
-                logText.text = "Sign in Failed! Unknown error.";
-                break;
-        }
-    }
-}
+//             default:
+//                 Debug.LogError("Unknown error: " + status);
+//                 logText.text = "Sign in Failed! Unknown error.";
+//                 break;
+//         }
+//     }
+// }
